74a6614c28eae31efb70ca8b9e638b2e41c6803d
patheard8 <87441787+pjheard8@users.noreply.github.com>
2023-02-28 17:56:14 -0500

    added routing for about and contact components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,11 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'about', component: AboutComponent},
+      {path: 'contact', component: ContactComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
